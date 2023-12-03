5836d41b721fea1a4f88fd023468804703e5eeef
Nour daly <nourhouda.dali01@gmail.com>
2023-12-03 06:02:31 +0100

    first

---
+++ src/router/index.js
@@ -0,0 +1,48 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import Contact from '../views/ContactView.vue'
+import Profil from '../views/ProfilView.vue'
+import Services from '../views/ServicesView.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/contact',
+   
+    component:Contact
+  },
+  {
+    path: '/profil',
+   
+    component:Profil
+  },
+ 
+  {
+    path: '/services',
+   
+    component:Services
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
